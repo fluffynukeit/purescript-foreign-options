be28f07cd2caf4a0b489f57daa2c34ff32be81d3
Alex Gryzlov <alexander.gryzlov@adform.com>
2016-03-22 18:20:11 +0200

    update for 0.8, don't use grunt

---
+++ src/Data/Foreign/Options.js
@@ -0,0 +1,45 @@
+/* global exports */
+"use strict";
+
+// module Data.Foreign.Options 
+exports.toOptionsImpl(l, r, j, n, o, record) { 
+  
+    var applyArgs = function(f, args) { 
+      return (args.length == 0) ? ((typeof f === 'function') ? f() : f) : applyArgs(f(args.shift()), args); 
+    }; 
+     
+    var convert = function(value) { 
+      var ctr = value.constructor; 
+      if (ctr === l.constructor || 
+          ctr === r.constructor || 
+          ctr === j.constructor) { 
+        return convert(value.value0); 
+      } else if (ctr === o.constructor) {
+        return value.value0; 
+      } else if (value === n) { 
+        return null; 
+      } else if (typeof value === 'function') { 
+        return function() { 
+                 var argArray = [].slice.call(arguments);
+                 return applyArgs(value, argArray); 
+               }; 
+      } else if (typeof value === 'object') { 
+        return toOptionsImpl(l, r, j, n, o, value); 
+      } else { 
+        return value; 
+      } 
+    }; 
+  
+    var optsRecord = {};
+    for (var field in record) { 
+      if (record.hasOwnProperty(field)) { 
+        var convValue = convert(record[field]); 
+        if (convValue != null) { 
+          optsRecord[field] = convValue; 
+        } 
+      } 
+    } 
+  
+    return optsRecord; 
+
+}
--- src/Data/Foreign/Options.purs
+++ src/Data/Foreign/Options.purs
@@ -1,4 +1,3 @@
-
 module Data.Foreign.Options 
   ( Options()
   , Opaque(..)
@@ -6,55 +5,14 @@ module Data.Foreign.Options
   ) 
 where
 
-import Data.Either
-import Data.Maybe
-import Data.Function
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..)) 
+import Data.Function (Fn6, runFn6)
 
 foreign import data Options :: * -> *
 data Opaque a = Opaque a
 
-foreign import toOptionsImpl """
-  function toOptionsImpl(l, r, j, n, o, record) { 
-  
-    var applyArgs = function(f, args) { 
-      return (args.length == 0) ? ((typeof f === 'function') ? f() : f) : applyArgs(f(args.shift()), args); 
-    }; 
-     
-    var convert = function(value) { 
-      var ctr = value.constructor; 
-      if (ctr === l.constructor || 
-          ctr === r.constructor || 
-          ctr === j.constructor) { 
-        return convert(value.value0); 
-      } else if (ctr === o.constructor) {
-        return value.value0; 
-      } else if (value === n) { 
-        return null; 
-      } else if (typeof value === 'function') { 
-        return function() { 
-                 var argArray = [].slice.call(arguments);
-                 return applyArgs(value, argArray); 
-               }; 
-      } else if (typeof value === 'object') { 
-        return toOptionsImpl(l, r, j, n, o, value); 
-      } else { 
-        return value; 
-      } 
-    }; 
-  
-    var optsRecord = {};
-    for (var field in record) { 
-      if (record.hasOwnProperty(field)) { 
-        var convValue = convert(record[field]); 
-        if (convValue != null) { 
-          optsRecord[field] = convValue; 
-        } 
-      } 
-    } 
-  
-    return optsRecord; 
-  }""" 
-  :: forall a b c.  
+foreign import toOptionsImpl :: forall a b c.  
      Fn6 (Either b b) (Either b b) (Maybe b) (Maybe b) (Opaque b) a c
 
 toOptions :: forall a. {|a} -> Options {|a}
